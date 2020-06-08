bef9e8740c8679929cb50caae8463d02d78e6c58
Avi Dessauer <avi.the.coder@gmail.com>
2020-06-08 19:10:08 -0400

    Mark soundness docs

--- src/mark.rs
+++ src/mark.rs
@@ -1,6 +1,10 @@
 use super::gc::*;
 use std::ops::Range;
 
+/// This will be sound once GAT or const Eq &str lands.
+/// The former will allow transmuting only lifetimes.
+/// The latter will make `assert_eq!(type_name::<O>(), type_name::<N>())` const.
+// https://github.com/rust-lang/rfcs/pull/2632.
 pub unsafe trait Mark<'o, 'n, 'r: 'n, O, N: 'r> {
     fn mark(&'n self, o: Gc<'o, O>) -> Gc<'r, N>;
 }
