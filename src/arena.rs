58957b781b3e5cad9c9d13df918583a905bc996a
Avi Dessauer <avi.the.coder@gmail.com>
2020-06-10 01:15:43 -0400

    Enum encoding of bus messages

--- src/arena.rs
+++ src/arena.rs
@@ -81,21 +81,19 @@ impl<T: Trace> Drop for ArenaInternals<T> {
             let tm = unsafe { &mut *tm.get() };
             tm.entry(key::<T>()).and_modify(|bus| {
                 let mut msgs = bus.lock().unwrap();
-                msgs[self.bus_idx as usize] = Some(BusMsg {
-                    from_gc: false,
-                    mem_addr: self.header as usize,
+                msgs[self.bus_idx as usize] = Msg::End {
+                    next: unsafe { *self.next.get() } as *const u8,
                     grey_feilds: unsafe { *self.grey_feilds.get() },
-                    // does not matter
-                    grey_self: true,
-                    white_region: (0, 1),
-                });
+                    white_start: self.white_region.start,
+                    white_end: self.white_region.end,
+                };
             });
         });
     }
 }
 
-unsafe impl<'o, 'n, 'r: 'n, O: NoGc + Immutable + 'o, N: NoGc + Immutable + 'r> Mark<'o, 'n, 'r, O, N>
-    for ArenaPrim<N>
+unsafe impl<'o, 'n, 'r: 'n, O: NoGc + Immutable + 'o, N: NoGc + Immutable + 'r>
+    Mark<'o, 'n, 'r, O, N> for ArenaPrim<N>
 {
     #[inline(always)]
     default fn mark(&'n self, o: Gc<'o, O>) -> Gc<'r, N> {
@@ -137,74 +135,83 @@ const fn header_size<T>() -> usize {
     header + ((align - (header % align)) % align)
 }
 
+/// Returns `header` and `next`.
+fn alloc_arena<T>() -> (*const Header<T>, *mut T) {
+    // Get more memory from system allocator
+    let mem_ptr = unsafe { System.alloc(Layout::new::<Mem>()) };
+    let mem_addr = mem_ptr as usize;
+    debug_assert!(
+        mem_addr
+            < unsafe { &std::mem::transmute::<_, &Mem>(mem_ptr)._mem[16383] } as *const _ as usize
+    );
+
+    let capacity = (16384 - header_size::<T>()) / size_of::<T>();
+    (
+        mem_addr as *const _,
+        (mem_addr + (capacity * size_of::<T>())) as *mut T,
+    )
+}
+
 impl<T: Trace> Arena<T> for ArenaInternals<T> {
     fn new() -> ArenaInternals<T> {
         let bus = GC_BUS.with(|tm| {
             let tm = unsafe { &mut *tm.get() };
             tm.entry(key::<T>())
-                .or_insert_with(|| Box::new(Mutex::new([None; 8])))
+                .or_insert_with(|| Box::new(Mutex::new([Msg::Slot; 8])))
         });
 
         let mut msgs = bus.lock().expect("Could not unlock bus");
         let slot = msgs
             .iter_mut()
             .enumerate()
-            .filter(|(_, o)| {
-                if let Some(m) = o {
-                    if m.from_gc {
-                        true
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            })
+            .filter(|(_, o)| o.is_gc())
             .next();
 
         if let Some((
             bus_idx,
-            &mut Some(BusMsg {
-                mem_addr,
+            &mut Msg::Gc {
+                next,
                 grey_self,
                 grey_feilds,
-                white_region,
-                ..
-            }),
+                white_start,
+                white_end,
+            },
         )) = slot
         {
-            msgs[bus_idx] = None;
-            let capacity = (16384 - header_size::<T>()) / size_of::<T>();
+            // FIXME make header a method
+            let header = ptr::null();
+            let next = UnsafeCell::new(if let Some(n) = next {
+                n as *mut T
+            } else {
+                alloc_arena::<T>().1
+            });
+
+            msgs[bus_idx] = Msg::Slot;
             ArenaInternals {
                 bus_idx: bus_idx as u8,
-                header: mem_addr as *const Header<T>,
-                next: UnsafeCell::new((mem_addr + (capacity * size_of::<T>())) as *mut T),
+                header,
+                next,
                 grey_self,
                 grey_feilds: UnsafeCell::new(grey_feilds),
-                white_region: white_region.0..white_region.1,
+                white_region: white_start..white_end,
             }
         } else {
-            // Get more memory from system allocator
+            let (header, next) = alloc_arena::<T>();
+
             let (bus_idx, slot) = msgs
-                .iter()
+                .iter_mut()
                 .enumerate()
-                .filter(|(_, o)| o.is_none())
+                .filter(|(_, o)| o.is_slot())
                 .next()
                 .expect("No space on bus left. Extending the bus not yet supported.");
-            let mem_ptr = unsafe { System.alloc(Layout::new::<Mem>()) };
-            let mem_addr = mem_ptr as usize;
-            debug_assert!(
-                mem_addr
-                    < unsafe { &std::mem::transmute::<_, &Mem>(mem_ptr)._mem[16383] } as *const _
-                        as usize
-            );
 
-            // TODO send start message
-            let capacity = (16384 - header_size::<T>()) / size_of::<T>();
+            // send start message to gc
+            *slot = Msg::Start(next as *const u8);
+
             ArenaInternals {
                 bus_idx: bus_idx as u8,
-                header: mem_addr as *const Header<T>,
-                next: UnsafeCell::new((mem_addr + (capacity * size_of::<T>())) as *mut T),
+                header,
+                next: UnsafeCell::new(next),
                 grey_self: false,
                 grey_feilds: UnsafeCell::new(0b0000_0000),
                 white_region: 0..1,
@@ -237,7 +244,7 @@ pub struct Header<T> {
 
 thread_local! {
     /// Map from type to GC communication bus.
-    static GC_BUS: UnsafeCell<HashMap<(GcTypeInfo, usize), Box<Mutex<[Option<BusMsg>; 8]>>>>
+    static GC_BUS: UnsafeCell<HashMap<(GcTypeInfo, usize), Box<Mutex<[Msg; 8]>>>>
         = UnsafeCell::new(HashMap::new());
 }
 
@@ -250,19 +257,43 @@ fn key<T: Trace>() -> (GcTypeInfo, usize) {
 
 // TODO replace with atomic 64 byte header encoding.
 // Plus variable length condemned region messages.
-// TODO allow multiple messages and therefore Arenas of T.
 #[derive(Debug, Copy, Clone)]
-pub struct BusMsg {
-    pub from_gc: bool,
-    pub mem_addr: usize,
-    /// Is a Self Arena being condemned.
-    grey_self: bool,
-    /// The 8 bits correspond to GCed fields.
-    /// Only 8 GCed fields per struct are supported.
-    /// TODO do enums with GCed fields count as one (conservative), or N fields.
-    /// TODO unpack bits from composite types tuples, inline structs, enums.
-    grey_feilds: u8,
-    /// TODO support multiple field specific regions in future encoding
-    /// Equivalents to Range<usize>. It's a tuple so it implements Copy
-    white_region: (usize, usize),
+enum Msg {
+    Slot,
+    /// The `mem_addr` of the area
+    Start(*const u8),
+    End {
+        /// Address of the 16 kB arena
+        next: *const u8,
+        grey_feilds: u8,
+        /// The condemned ptr range
+        /// Will be replaced with epoch
+        white_start: usize,
+        white_end: usize,
+    },
+    Gc {
+        next: Option<*mut u8>,
+        grey_feilds: u8,
+        grey_self: bool,
+        /// The condemned ptr range
+        white_start: usize,
+        white_end: usize,
+    },
+}
+
+impl Msg {
+    #[inline(always)]
+    fn is_slot(&self) -> bool {
+        match self {
+            Msg::Slot => true,
+            _ => false,
+        }
+    }
+
+    fn is_gc(&self) -> bool {
+        match self {
+            Msg::Gc { .. } => true,
+            _ => false,
+        }
+    }
 }
