3c785b265997adf74b1cc1f3bbda596e99dfaa10
Avi Dessauer <avi.the.coder@gmail.com>
2020-07-10 20:40:09 -0400

    Switch to shared cargo workspace

---
+++ sundial-gc/tests/drop_test.rs
@@ -0,0 +1,58 @@
+#![allow(incomplete_features)]
+#![feature(optin_builtin_traits)]
+#![feature(const_generics)]
+#![feature(untagged_unions)]
+#![feature(const_fn)]
+#![feature(const_transmute)]
+#![feature(specialization)]
+#![feature(negative_impls)]
+#![feature(const_raw_ptr_to_usize_cast)]
+#![feature(const_mut_refs)]
+#![feature(trivial_bounds)]
+#![feature(associated_type_bounds)]
+#![feature(const_panic)]
+
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    thread,
+    time::Duration,
+};
+use sundial_gc::arena::*;
+use sundial_gc::TRIGGER_MAJOR_GC;
+
+fn log_init() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
+static COUNT: AtomicUsize = AtomicUsize::new(0);
+struct Count(usize);
+
+impl Count {
+    pub fn new() -> Self {
+        COUNT.fetch_add(1, Ordering::Relaxed);
+        Count(1)
+    }
+}
+
+impl Drop for Count {
+    fn drop(&mut self) {
+        COUNT.fetch_sub(1, Ordering::Relaxed);
+    }
+}
+
+#[test]
+fn drop_test() {
+    log_init();
+    for _ in 0..1000 {
+        let a = Arena::new();
+        for _ in 0..100 {
+            a.gc_alloc(Count::new());
+        }
+    }
+
+    TRIGGER_MAJOR_GC.store(true, Ordering::Relaxed);
+    while COUNT.load(Ordering::Relaxed) != 0 {
+        eprintln!("Count {}", COUNT.load(Ordering::Relaxed));
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
