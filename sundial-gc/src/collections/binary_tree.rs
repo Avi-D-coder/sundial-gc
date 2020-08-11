1c0cfc79cea275e0de263f2f1bb19530a1d23f25
Avi Dessauer <avi.the.coder@gmail.com>
2020-08-11 07:15:27 -0400

    Derive Trace for BinaryTree

--- sundial-gc/src/collections/binary_tree.rs
+++ sundial-gc/src/collections/binary_tree.rs
@@ -1,6 +1,14 @@
-use crate::{Gc, Trace};
+use crate as sundial_gc;
+use std::cmp::Ordering::*;
+use sundial_gc::*;
+use sundial_gc_derive::*;
 
-pub enum BinaryTree<'r, K: 'r, V: 'r> {
+#[derive(Debug, Copy, Clone, Trace)]
+pub enum BinaryTree<'r, K, V>
+where
+    K: 'r,
+    V: 'r,
+{
     Empty,
     Branch(Gc<'r, (K, Self, Self, V)>),
 }
@@ -10,46 +18,10 @@ impl<'r, K: Ord, V> BinaryTree<'r, K, V> {
         match self {
             BinaryTree::Empty => None,
             BinaryTree::Branch(Gc((k, l, r, v), ..)) => match key.cmp(k) {
-                std::cmp::Ordering::Equal => Some(v),
-                std::cmp::Ordering::Less => l.get(key),
-                std::cmp::Ordering::Greater => r.get(key),
+                Equal => Some(v),
+                Less => l.get(key),
+                Greater => r.get(key),
             },
         }
     }
-}
-
-unsafe impl<'r, K: 'r, V: 'r> Trace for BinaryTree<'r, K, V> {
-    fn fields(s: &Self, offset: u8, grey_fields: u8, invariant: &crate::mark::Invariant) -> u8 {
-        let mut bloom = 0b0000000;
-        match s {
-            BinaryTree::Empty => (),
-            BinaryTree::Branch(gc) => bloom |= Trace::fields(gc, offset, grey_fields, invariant),
-        }
-        bloom
-    }
-    unsafe fn evacuate<'e>(
-        s: &Self,
-        offset: crate::mark::Offset,
-        grey_fields: u8,
-        invariant: &crate::mark::Invariant,
-        handlers: &mut crate::mark::Handlers,
-    ) {
-        match s {
-            BinaryTree::Empty => (),
-            BinaryTree::Branch(gc) => {
-                Gc::<'r, (K, Self, Self, V)>::evacuate(gc, offset, grey_fields, invariant, handlers)
-            }
-        }
-    }
-    fn direct_gc_types(
-        t: &mut std::collections::HashMap<crate::mark::GcTypeInfo, crate::mark::TypeRow>,
-        offset: u8,
-    ) {
-        Gc::<'r, (K, Self, Self, V)>::direct_gc_types(t, offset)
-    }
-    fn transitive_gc_types(tti: *mut crate::mark::Tti) {
-        Gc::<'r, (K, Self, Self, V)>::transitive_gc_types(tti)
-    }
-    const GC_COUNT: u8 = Gc::<'r, (K, Self, Self, V)>::GC_COUNT;
-    const PRE_CONDTION: bool = Gc::<'r, (K, Self, Self, V)>::PRE_CONDTION;
 }
