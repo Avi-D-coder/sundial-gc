c5865c58ba2c4eaef67d49264646cb5caaa16d5d
Avi Dessauer <avi.the.coder@gmail.com>
2020-08-20 18:36:15 -0400

    Improve tiny_lang example

--- sundial-gc/examples/tiny_fp_lang.rs
+++ sundial-gc/examples/tiny_fp_lang.rs
@@ -1,119 +1,259 @@
 #![allow(incomplete_features)]
 #![feature(specialization)]
 
-use collections::Map;
-use sundial_gc::*;
+use rustyline::Editor;
+use sundial_gc::{collections::Map, *};
 use sundial_gc_derive::*;
 
-fn main() {}
+fn main() {
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
 
-// #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord,)]
-// enum Tokens {
-//     Text,
-//     Bracket,
-//     Operator
-// }
+    let mut partial = String::default();
+    let mut global = Env::default();
+    loop {
+        let readline = rl.readline("λ ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                partial.extend(line.chars());
 
-// fn parse<'r>(expr: &str, arena: &Arena<Expr<'r>>) -> Result<Expr<'r>, String> {
-//     expr.chars().filter(|c| !c.is_whitespace()).group_by();
-//     todo!()
-// }
+                let strs = Arena::new();
+                let exprs = Arena::new();
+                let nodes = Arena::new();
+                if let Ok((_, term)) = parse::term(&partial, &strs, &exprs, &nodes) {
+                    eprintln!("{:?}", term);
+                    match term {
+                        Term::Fun(name, expr) => {
+                            global = global.insert(name, eval(global, expr), &Arena::new());
+                        }
+                        Term::Exp(expr) => {
+                            println!("{:?}", eval(global, expr));
+                        }
+                    };
+                    partial.clear();
+                };
+            }
+            Err(_) => {
+                break;
+            }
+        }
+        global.iter().for_each(|kv|  eprintln!("{:?}", kv));
+    }
+}
 
 mod parse {
-    use crate::{Expr, GcExpr, Ident, Ty};
+    use crate::{Expr, GcExpr, Ident, Op, Term, Ty};
     use nom::{
         branch,
-        bytes::complete::is_not,
-        // see the "streaming/complete" paragraph lower for an explanation of these submodules
-        character::complete::{alphanumeric1, char, digit1, space1},
+        bytes::streaming::is_not,
+        character::complete::{alphanumeric1, char, digit1, space0, space1},
         do_parse,
+        error::ErrorKind,
         map,
-        named,
-        named_args,
         sequence::delimited,
-        tag,
-        IResult,
+        tag, IResult,
     };
-    use sundial_gc::Arena;
+    use sundial_gc::{
+        collections::{Map, Node},
+        Arena,
+    };
 
     fn parens(input: &str) -> IResult<&str, &str> {
         delimited(char('('), is_not(")"), char(')'))(input)
     }
 
-    fn ty<'a, 'r>(arena: &'r Arena<String>, input: &'a str) -> IResult<&'a str, Ty<'r>> {
+    fn ty<'a, 's, 'r: 's>(arena: &'s Arena<String>, input: &'a str) -> IResult<&'a str, Ty<'r>> {
+        dbg!(input);
         let string = |i| {
             map!(i, delimited(char('"'), is_not("\""), char('"')), |s| {
                 Ty::Str(arena.gc(String::from(s)))
             })
         };
 
-        let int = |i| map!(i, digit1, |i: &str| Ty::Int(i.parse().unwrap()));
+        let int = |i: &'a str| do_parse!(i, d: digit1 >> (Ty::Int(d.parse().unwrap())));
 
-        let tru = |i| map!(i, tag!("True"), |_| Ty::Bool(true));
-        let fal = |i| map!(i, tag!("False"), |_| Ty::Bool(false));
+        let tru = |i: &'a str| do_parse!(i, tag!("True") >> (Ty::Bool(true)));
+        let fal = |i: &'a str| do_parse!(i, tag!("False") >> (Ty::Bool(false)));
 
-        branch::alt((int, tru, fal, string))(input)
+        dbg!(branch::alt((int, tru, fal, string))(input))
     }
 
-    fn ident<'a, 'r>(input: &'a str, arena: &'r Arena<String>) -> IResult<&'a str, Ident<'r>> {
-        map!(input, alphanumeric1, |i| arena.gc(String::from(i)))
+    #[test]
+    fn ty_test() {
+        assert_eq!(ty(&Arena::new(), "1"), Ok(("", Ty::Int(1))));
+        assert_eq!(ty(&Arena::new(), "True"), Ok(("", Ty::Bool(true))));
+        assert_eq!(ty(&Arena::new(), "False"), Ok(("", Ty::Bool(false))));
+        assert_eq!(
+            ty(&Arena::new(), "\"Foo\""),
+            Ok(("", Ty::Str(Arena::new().gc(String::from("Foo")))))
+        );
     }
 
-    fn bind<'a, 'r>(
+    fn ident<'a, 's, 'r: 's>(
         input: &'a str,
-        strs: &'r Arena<String>,
-        exprs: &'r Arena<Expr<'r>>,
+        arena: &'s Arena<String>,
+    ) -> IResult<&'a str, Ident<'r>> {
+        dbg!(input);
+        // Identifiers must start with a letter.
+        if let Some(c) = input.chars().next() {
+            if !c.is_lowercase() {
+                return Err(nom::Err::Error(("Not a identifier", ErrorKind::NoneOf)));
+            };
+        };
+
+        dbg!(map!(input, alphanumeric1, |i| arena.gc(String::from(i))))
+    }
+
+    fn bind<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
+        input: &'a str,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
     ) -> IResult<&'a str, GcExpr<'r>> {
+        dbg!(input);
         let ident = |i| ident(i, strs);
 
-        let expr = |i| expr(i, strs, exprs);
+        let expr = |i| expr(i, strs, exprs, nodes);
         do_parse!(
             input,
-            tag!("let") >> space1 >> n: ident >> space1 >> e: expr >> (exprs.gc(Expr::Bind(n, e)))
+            tag!("let")
+                >> space1
+                >> n: ident
+                >> space0
+                >> tag!("=")
+                >> space0
+                >> e: expr
+                >> (exprs.gc(Expr::Bind(n, e)))
         )
     }
 
-    fn lam<'a, 'r>(
+    fn lam<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
         input: &'a str,
-        strs: &'r Arena<String>,
-        exprs: &'r Arena<Expr<'r>>,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
     ) -> IResult<&'a str, GcExpr<'r>> {
-        let expr = |i| expr(i, strs, exprs);
+        dbg!(input);
+        let expr = |i| expr(i, strs, exprs, nodes);
 
         do_parse!(
             input,
-            tag!("\\") >> space1 >> parens >> space1 >> e: expr >> (exprs.gc(Expr::Lam(e)))
+            tag!("\\") >> space0 >> parens >> space0 >> e: expr >> (exprs.gc(Expr::Lam(e)))
         )
     }
 
-    fn app<'a, 'r>(
+    fn app<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
         input: &'a str,
-        strs: &'r Arena<String>,
-        exprs: &'r Arena<Expr<'r>>,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
     ) -> IResult<&'a str, GcExpr<'r>> {
-        let expr = |i| expr(i, strs, exprs);
+        dbg!(input);
+        let expr = |i| expr(i, strs, exprs, nodes);
+        let ident = |i| ident(i, strs);
 
-        let args = do_parse!(input, e: expr >> (parens));
+        let (input, (func, mut args)) = do_parse!(input, e: expr >> p: parens >> (e, p))?;
 
-        // To curry or not to curry that is the question.
-        todo!()
+        let mut parms = Map::default();
+        loop {
+            args = space0(args)?.0;
+            if args.is_empty() {
+                break;
+            }
+
+            let (remaining, (p, e)) = do_parse!(
+                args,
+                param: ident >> space0 >> tag!(":") >> space0 >> e: expr >> (param, e)
+            )?;
+            args = remaining;
+            parms = parms.insert(p, e, nodes)
+        }
+
+        Ok((input, exprs.gc(Expr::App(func, parms))))
     }
 
-    fn expr<'a, 'r>(
+    fn prim<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
         input: &'a str,
-        strs: &'r Arena<String>,
-        exprs: &'r Arena<Expr<'r>>,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
     ) -> IResult<&'a str, GcExpr<'r>> {
+        dbg!(input);
         let lit = |i| map!(i, |i| ty(strs, i), |ty| exprs.gc(Expr::Lit(ty)));
+        let (input, l) = branch::alt((lit, |i| expr(i, strs, exprs, nodes)))(input)?;
+        dbg!(input, l);
+        let (input, _) = space0(input)?;
 
-        let var = |i| map!(i, |i| ident(i, strs), |n| exprs.gc(Expr::Var(n)));
-        let exp = |i| expr(i, strs, exprs);
-        let lam = |i| lam(i, strs, exprs);
-        let bind = |i| bind(i, strs, exprs);
-        let app = |i| app(i, strs, exprs);
+        let (input, op) = if input.starts_with("+") {
+            (&input[1..], Op::Add)
+        } else if input.starts_with("-") {
+            (&input[1..], Op::Sub)
+        } else if input.starts_with("*") {
+            (&input[1..], Op::Mul)
+        } else if input.starts_with("/") {
+            (&input[1..], Op::Div)
+        } else if input.starts_with("==") {
+            (&input[2..], Op::Eql)
+        } else {
+            return Err(nom::Err::Error(("No PrimOp", ErrorKind::NoneOf)));
+        };
 
-        branch::alt((lit, bind, lam, var, app))(input)
+        dbg!(input, op);
+        let (input, r) = expr(input, strs, exprs, nodes)?;
+        dbg!(input, r);
+
+        Ok((input, exprs.gc(Expr::Prim(op, l, r))))
     }
+
+    pub(crate) fn expr<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
+        input: &'a str,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
+    ) -> IResult<&'a str, GcExpr<'r>> {
+        dbg!(input);
+        let lit = |i| map!(i, |i| ty(strs, i), |ty| exprs.gc(Expr::Lit(ty)));
+        let var = |i| map!(i, |i| ident(i, strs), |n| exprs.gc(Expr::Var(n)));
+        let lam = |i| lam(i, strs, exprs, nodes);
+        let app = |i| app(i, strs, exprs, nodes);
+        let bind = |i| bind(i, strs, exprs, nodes);
+        let prim = |i| prim(i, strs, exprs, nodes);
+
+        let (input, _) = space0(input)?;
+        dbg!(branch::alt((bind, lam, var, prim, lit, app))(input))
+    }
+
+    pub(crate) fn term<'a, 's, 'e, 'n, 'r: 's + 'e + 'n>(
+        input: &'a str,
+        strs: &'s Arena<String>,
+        exprs: &'e Arena<Expr<'r>>,
+        nodes: &'n Arena<Node<'r, Ident<'r>, GcExpr<'r>>>,
+    ) -> IResult<&'a str, Term<'r>> {
+        dbg!(input);
+        let term = |i: &'a str| {
+            let expr = |i| expr(i, strs, exprs, nodes);
+            let ident = |i| ident(i, strs);
+            do_parse!(
+                i,
+                n: ident >> space0 >> tag!("=") >> space0 >> e: expr >> (Term::Fun(n, e))
+            )
+        };
+
+        let expr = |i: &'a str| {
+            let expr = |i| expr(i, strs, exprs, nodes);
+            do_parse!(i, e: expr >> (Term::Exp(e)))
+        };
+
+        let (input, _) = space0(input)?;
+        branch::alt((term, expr))(input)
+    }
+}
+
+#[derive(Trace, Debug, Copy, Clone, Eq, PartialEq)]
+enum Term<'r> {
+    Fun(Ident<'r>, GcExpr<'r>),
+    Exp(GcExpr<'r>),
 }
 
 #[derive(Trace, Debug, Copy, Clone, Eq, PartialEq)]
@@ -132,7 +272,9 @@ type Ident<'r> = Gc<'r, String>;
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Op {
     Add,
+    Sub,
     Mul,
+    Div,
     Eql,
 }
 
@@ -140,7 +282,9 @@ impl Op {
     fn eval<'r>(self, a: Value<'r>, b: Value<'r>) -> Value<'r> {
         match (self, a, b) {
             (Op::Add, Type(Int(a)), Type(Int(b))) => Type(Int(a + b)),
+            (Op::Sub, Type(Int(a)), Type(Int(b))) => Type(Int(a - b)),
             (Op::Mul, Type(Int(a)), Type(Int(b))) => Type(Int(a * b)),
+            (Op::Div, Type(Int(a)), Type(Int(b))) => Type(Int(a / b)),
             (Op::Eql, Type(a), Type(b)) => Type(Bool(a == b)),
             _ => panic!("Hit unevaluated closure"),
         }
@@ -167,7 +311,7 @@ type Env<'r> = Map<'r, Gc<'r, String>, Value<'r>>;
 fn eval<'r>(env: Env<'r>, term: Gc<'r, Expr<'r>>) -> Value<'r> {
     match *term {
         Expr::Lit(ty) => Type(ty),
-        Expr::Var(n) => env[&n],
+        Expr::Var(n) => *env.get(&n).unwrap_or_else(|| panic!("Error: {} is not in scope!", *n)),
         Expr::Lam(a) => Value::Closure(a, env),
         Expr::App(l, args) => match eval(env, l) {
             Type(ty) => panic!("{:?} is not a function", ty),
